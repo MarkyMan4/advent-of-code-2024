4ddce865bc480d293fabfe72827e7318f7edb44d
MarkyMan4 <markus.hutnik@gmail.com>
2024-12-01 23:11:26 -0600

    day 1

---
+++ day1/part1/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/*
+take a line from the file and return the two integers
+*/
+func splitLine(line string) (int, int) {
+	nums := strings.Split(line, "   ")
+	num1, _ := strconv.Atoi(nums[0])
+	num2, _ := strconv.Atoi(nums[1])
+
+	return num1, num2
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	left := []int{}
+	right := []int{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		num1, num2 := splitLine(line)
+		left = append(left, num1)
+		right = append(right, num2)
+	}
+
+	sort.Slice(left, func(i, j int) bool {
+		return left[i] < left[j]
+	})
+	sort.Slice(right, func(i, j int) bool {
+		return right[i] < right[j]
+	})
+
+	totalDiff := 0
+	for i := 0; i < len(left); i++ {
+		diff := int(math.Abs(float64(left[i]) - float64(right[i])))
+		totalDiff += diff
+	}
+
+	fmt.Println(totalDiff)
+}
