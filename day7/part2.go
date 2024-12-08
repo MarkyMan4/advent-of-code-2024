ec1f5447d7a47a03e719c7c127e4a1ebd6bda3b6
MarkyMan4 <markus.hutnik@gmail.com>
2024-12-07 23:23:02 -0600

    day 7 part 2

--- day7/part2.go
+++ day7/part2.go
@@ -1,5 +1,41 @@
 package day7
 
-func SolvePart2() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func SolvePart2() {
+	file, err := os.Open("input/day7input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	equations := []equation{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		testVal, operands := parseEquation(line)
+		equations = append(equations, equation{testVal, operands})
+	}
+
+	chars := []rune{plus, mul, concat}
+	sum := 0
+
+	for _, eq := range equations {
+		operatorOptions := generateOperandPermutations(len(eq.operands)-1, chars)
+
+		for _, opList := range operatorOptions {
+			if evaulate(eq.operands, opList) == eq.testVal {
+				sum += eq.testVal
+				break
+			}
+		}
+	}
+
+	fmt.Println(sum)
 }
